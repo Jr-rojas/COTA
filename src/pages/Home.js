70d3f2fcc80ba051c151054a33874a61ca42ced8
Jersain Rojas <j.rojas_7@hotmail.com>
2023-07-09 18:42:44 -0700

    More Sections added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,8 @@ export default function Home(){
     return(
         <>
             <Hero title="Bienvenidos"/>
+
+            {/* NUESTRA HISTORIA SECTION */}
             <section className={styles.section}>
                 <div className={styles.container}>
                     <div className={styles.content}>
@@ -20,6 +22,8 @@ export default function Home(){
                     <div className={styles.img}/>
                 </div>
             </section>
+
+            {/* COTA UNITED SECTION */}
             <section className={fanStyles.section}>
                 <div className={fanStyles.container}>
                     <div className={fanStyles.img}></div>
@@ -39,7 +43,9 @@ export default function Home(){
                     </div>
                 </div>
             </section>
-            <section className={fanStyles.fanSection}>
+
+            {/* EVENTS SECTION */}
+            <section className={fanStyles.fanSection} >
                 <div className='container'>
                     <div className={styles.content}>
                         <h4>Acompañanos</h4>
@@ -98,6 +104,42 @@ export default function Home(){
                                     <span className='buttons hover-dark'><a href='/events'>Mas Detalles</a></span>
                                 </span>
                             </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* TOMA TU LLAMADO */}
+            <section className={fanStyles.sectionB}>
+                <div className={fanStyles.containerR}>
+                    <div className={fanStyles.contentR}>
+                        <div className={fanStyles.innerR}>
+                            <div className={fanStyles.heading}>
+                                <h4 className={styles.h4}>TOMA TU</h4>
+                                <h1 className={fanStyles.h1}>LLAMADO</h1>
+                            </div>
+                            <hr className={fanStyles.hrR}/>
+                            <p className={styles.p}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+                            <div className='buttons la hover-dark'>
+                                <a href='/' >Detalles</a>
+                                <a href='/' >sembrar</a>
+                            </div>
+                        </div> 
+                    </div>
+                    <div className={fanStyles.imgR}></div>
+                </div>
+            </section>
+            {/* COTA KIDS & COTA YOUTH */}
+            <section className={styles.sectionB}>
+                <div className={styles.container2}>
+                    <div className={styles.img2}/>
+                    <div className={styles.content2}>
+                        <h1 className={styles.h1}>COTA Kids & Cota Youth</h1>
+                        <hr className={styles.hr2}/>
+                        <p className={styles.p}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <div className='buttons la hover-light'>
+                            <a href="/">Kids</a>
+                            <a href="/">Youth</a>
                         </div>
                     </div>
                 </div>
