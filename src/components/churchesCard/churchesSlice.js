6022b4817cc58de98a4ede2261780bb0fb89cc58
Jersain Rojas <j.rojas_7@hotmail.com>
2025-05-30 21:52:15 -0700

    Cleaned up old folders from root

--- src/components/churchesCard/churchesSlice.js
+++
@@ -1,5 +0,0 @@
-import { CHURCHES } from "../../app/shared/data/CHURCHES"
-
-export const selectAllChurches = () => {
-    return CHURCHES;
-}
--- src/utils/formatters.js
+++
@@ -1,5 +0,0 @@
-export const formatChurchName = (churchName) => {
-    return churchName.replace(/(COTA-)(.+)/, (match, prefix, namePart) => 
-        prefix + namePart.replace(/-/g, " ")
-    );
-};
